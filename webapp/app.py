0810e7c71ca0eb3fa0637e58412c1126d4531b4a
Mrinal Sinha <mail@themrinalsinha.com>
2018-01-31 00:55:32 +0530

    setting connection to database and pull data limited to 100

--- webapp/app.py
+++ webapp/app.py
@@ -2,6 +2,14 @@ from flask   import Flask, render_template
 from sqlite3 import connect
 from os      import path
 
+path = path.dirname(path.realpath(__file__))
+app  = Flask(__name__, static_folder = path, template_folder = path)
+db   = path + '/allitebooks.db'
+
+connection = connect(db)
+cursor     = connection.cursor()
+data       = cursor.execute("SELECT category, book_name, cover_img, authors, pages, \
+                             description, file_size, download_link FROM ebooks_index LIMIT 100").fetchall()
 
 @app.route('/')
 def index():
