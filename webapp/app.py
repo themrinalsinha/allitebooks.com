bf4b630f81d4c980320ab4273020c7414e309b95
Mrinal Sinha <mail@themrinalsinha.com>
2018-01-31 00:54:24 +0530

    setting setting route

--- webapp/app.py
+++ webapp/app.py
@@ -3,5 +3,9 @@ from sqlite3 import connect
 from os      import path
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', result = data)
+
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
